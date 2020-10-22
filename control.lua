fb426713c1341fe583809ee7c8a14337b77a1169
Konstantin Gainullin <konstg.dev@gmail.com>
2020-10-22 22:06:55 +0300

    typo

--- control.lua
+++ control.lua
@@ -69,9 +69,9 @@ script.on_event(defines.events.on_tick, function(event)
       end
       if not state.disabled and state.entity.active then
         if state.clock % 10 == 0 then
-          state.out_out_power = not sufficient_power(state.entity)
+          state.out_of_power = not sufficient_power(state.entity)
         end
-        if not state.out_out_power then
+        if not state.out_of_power then
           if state.modified then
             Controller.compile(state)
             Controller.set_program_counter(state, state.instruction_pointer)
