cb12c62915f3ff0fdab2b4c2d9a2bfb1c333953d
Konstantin Gainullin <konstg.dev@gmail.com>
2020-11-12 05:52:08 +0300

    take care of broken mods shit

--- control.lua
+++ control.lua
@@ -133,16 +133,14 @@ local function on_entity_settings_pasted(event)
 end
 
 local function setup_blueprint_tag(blueprint, index, entity)
-  if entity.name == 'fcpu' then
-    local state = get_fcpu_state(entity)
-    if state then
-      blueprint.set_blueprint_entity_tag(index, "fcpu", {
-        t = state.program_text,
-        i = state.instruction_pointer,
-        r = Controller.is_running(state),
-        d = state.disabled
-      })
-    end
+  local state = get_fcpu_state(entity)
+  if state then
+    blueprint.set_blueprint_entity_tag(index, 'fcpu', {
+      t = state.program_text,
+      i = state.instruction_pointer,
+      r = Controller.is_running(state),
+      d = state.disabled
+    })
   end
 end
 
@@ -152,20 +150,43 @@ local function on_player_setup_blueprint(event)
     local blueprint = nil
     if player.blueprint_to_setup and player.blueprint_to_setup.valid_for_read then
       blueprint = player.blueprint_to_setup
-    elseif player.cursor_stack and player.cursor_stack.valid_for_read and player.cursor_stack.name == "blueprint" then
+    elseif player.cursor_stack and player.cursor_stack.valid_for_read and player.cursor_stack.name == 'blueprint' then
       blueprint = player.cursor_stack
     end
+
     if blueprint then
-      local t = event.mapping.get()
-      if game.active_mods['attach-notes'] then
-        local ents = blueprint.get_blueprint_entities()
-        if 0 < #ents and #t == 0 then
-          player.print({"fcpu-errors.blueprint-broken", "Attach Notes"}, {r=0.5, g=0.5})
-          player.print({"fcpu-errors.blueprint-unavailable", "Attach Notes"}, {r=0.5})
+      local mapping = event.mapping.get()
+      local entities = blueprint.get_blueprint_entities()
+
+      if #mapping == 0 and 0 < #entities then
+        local pos2idx = {}
+        for _,v in ipairs(entities) do
+          if v.name == 'fcpu' then
+            local pos = v.position.x ..';'.. v.position.y
+            pos2idx[pos] = v.emtity_number
+          end
         end
-      end
-      for index, entity in pairs(t) do
-        setup_blueprint_tag(blueprint, index, entity)
+
+        if next(pos2idx) then
+          --if game.active_mods['attach-notes'] then
+          --  player.print({"fcpu-errors.blueprint-broken", "Attach Notes"}, {r=0.5, g=0.5})
+          --  player.print({"fcpu-errors.blueprint-unavailable", "Attach Notes"}, {r=0.5})
+          --end
+          local ents = player.surface.find_entities_filtered{name = 'fcpu', area = event.area, force = player.force}
+          for _,ent in ipairs(ents) do
+            local pos = ent.position.x ..';'.. ent.position.y
+            local idx = pos2idx[pos]
+            if idx then
+              setup_blueprint_tag(blueprint, idx, ent)
+            end
+          end
+        end
+      else
+        for idx, ent in pairs(mapping) do
+          if ent.name == 'fcpu' then
+            setup_blueprint_tag(blueprint, idx, ent)
+          end
+        end
       end
     end
   end
